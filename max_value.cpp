28a9911d8e1b31265a8d57699a982d50fda851ce
Antipozitife <Antipozitife@icloud.com>
2024-05-14 19:59:35 +0300

    task

--- max_value.cpp
+++ max_value.cpp
@@ -4,31 +4,6 @@
 #include<queue>
 using namespace std;
 
-int main()
-{
-    ifstream in("/Users/alinagaripova/Downloads/задача про максимальное значение/input1.txt");
-
-    int n = 0;
-    in >> n;
-
-    double* nums = new double[n];
-    for (int i = 0; i < n; i++) {
-        in >> nums[i];
-    }
-
-    in.close();
-
-    double result = calculateMaxValue(nums, n);
-    delete[] nums;
-
-    ofstream out("/Users/alinagaripova/Downloads/задача про максимальное значение/output1.txt");
-    cout << result << endl;
-    out << result << endl;
-    out.close();
-
-    return 0;
-}
-
 double calculateMaxValue(double* nums, int n){
     std::queue<double> q;
     
